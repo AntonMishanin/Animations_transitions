35f38cfc7a6227d356dab863c9189c4d43f60caf
Anton Mishanin <antonmishanin@mail.ru>
2022-03-29 10:36:33 +0700

    reveal or hide view: card flip

--- app/src/main/java/com/my/my_animation_example/MainActivity.kt
+++ app/src/main/java/com/my/my_animation_example/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.my.my_animation_example
 
 import android.animation.*
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageView
+import androidx.core.animation.doOnEnd
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +16,9 @@ class MainActivity : AppCompatActivity() {
     private val someObjectForLinear by lazy { findViewById<ImageView>(R.id.some_object_for_linear) }
     private val objectForRound by lazy { findViewById<ImageView>(R.id.some_object_for_round) }
     private val objectWithListOfImage by lazy { findViewById<ImageView>(R.id.some_list_of_image) }
+
+    private val cardFront by lazy { findViewById<ImageView>(R.id.card_front) }
+    private val cardBack by lazy { findViewById<ImageView>(R.id.card_back) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +31,10 @@ class MainActivity : AppCompatActivity() {
 
         someObjectForLinear.setOnClickListener {
             crossFade()
+        }
+
+        cardFront.setOnClickListener {
+            flipCard(applicationContext, visibleView = cardBack, invisibleView = cardFront)
         }
     }
 
@@ -51,5 +61,34 @@ class MainActivity : AppCompatActivity() {
                     objectForRound.visibility = View.GONE
                 }
             })
+    }
+
+    private fun flipCard(context: Context, visibleView: View, invisibleView: View) {
+        try {
+            visibleView.isVisible = true
+            val scale = context.resources.displayMetrics.density
+            val cameraDist = 8000 * scale
+            visibleView.cameraDistance = cameraDist
+            invisibleView.cameraDistance = cameraDist
+            val flipOutAnimatorSet =
+                AnimatorInflater.loadAnimator(
+                    context,
+                    R.animator.flip_out
+                ) as AnimatorSet
+            flipOutAnimatorSet.setTarget(invisibleView)
+            val flipInAnimatorSet =
+                AnimatorInflater.loadAnimator(
+                    context,
+                    R.animator.flip_in
+                ) as AnimatorSet
+            flipInAnimatorSet.setTarget(visibleView)
+            flipOutAnimatorSet.start()
+            flipInAnimatorSet.start()
+            flipInAnimatorSet.doOnEnd {
+                invisibleView.isVisible = false
+            }
+        } catch (e: Exception) {
+            print(e.message)
+        }
     }
 }
